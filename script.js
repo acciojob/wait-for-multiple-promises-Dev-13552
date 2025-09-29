6a0dd95dfccd4e381a983d81229dcc9251cb9872
Dev-13552 <devrathour110@gmail.com>
2025-09-29 15:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const table = document.querySelector("#output")
 
 function randomTime(){
-	return Math.floor(Math.random()*4)+1
+	return Math.floor(Math.random()*4)
 }
 
 const promise1 = new Promise((res, rej)=>{
@@ -30,14 +30,21 @@ const promise3 = new Promise((res, rej)=>{
 
 Promise.all([promise1, promise2,promise3])
 .then((results)=>{
-	
+	let total = 0
+	const loading = document.querySelector("#loading")
+	loading.remove()
 	results.forEach((result, index)=>{
+		total += result
 		const row = document.createElement('tr')
 		row.innerHTML = `<td>Promise ${index+1}</td><td>${result}</td>`
 		table.appendChild(row)
 	})
 
-
+		const row = document.createElement('tr')
+	
+		row.innerHTML = `<td>Total</td><td>${total}</td>`
+		table.appendChild(row)
+		
 	
 	
 })
