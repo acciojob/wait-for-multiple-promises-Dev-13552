cde262628f8f712afc6041441fd5b813fe4a92ec
Dev-13552 <devrathour110@gmail.com>
2025-09-29 15:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const table = document.querySelector("#output")
+
+function randomTime(){
+	return Math.floor(Math.random()*4)+1
+}
+
+const promise1 = new Promise((res, rej)=>{
+	const time = randomTime()
+	setTimeout(()=>{
+		res(time)
+	}, time)
+})
+
+
+const promise2 = new Promise((res, rej)=>{
+	const time = randomTime()
+	setTimeout(()=>{
+		res(time)
+	}, time)
+})
+
+
+const promise3 = new Promise((res, rej)=>{
+	const time = randomTime()
+	setTimeout(()=>{
+		res(time)
+	}, time)
+})
+
+Promise.all([promise1, promise2,promise3])
+.then((results)=>{
+	
+	results.forEach((result, index)=>{
+		const row = document.createElement('tr')
+		row.innerHTML = `<td>Promise ${index+1}</td><td>${result}</td>`
+		table.appendChild(row)
+	})
+
+
+	
+	
+})
+
